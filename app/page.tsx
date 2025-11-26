1fc996123e71de20b7414c505234b3bd814339a8
SantosJanine <infinitech.janine@gmail.com>
2025-11-26 13:41:52 +0800

    Updated Files

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import TestimonialsSection from "@/components/home/testimonials-section"
 import ContactSection from "@/components/home/contact-section"
 import SectionDivider from "@/components/home/section-divider"
 import RoomOffersSection from "@/components/home/offers-section"
-import RoomPreviewSection from "@/components/RoomPreviewSection"
+import RoomPreviewSection from "@/components/home/RoomPreviewSection"
 
 export default function HomePage() {
   return (
